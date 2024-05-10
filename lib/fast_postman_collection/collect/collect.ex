e2b1a0532cade41438e53911dfef50e2a3e7e57f
kalachlegends <artem.kalachev.2002@gmail.com>
2024-05-10 10:20:27 +0500

    feat: collect

--- lib/fast_postman_collection/collect/collect.ex
+++ lib/fast_postman_collection/collect/collect.ex
@@ -1,4 +1,5 @@
 defmodule FastPostmanCollection.Collect do
+  require Logger
   alias FastPostmanCollection.{CollectDataItem, CollectDataModule}
 
   def generate_data_by_router(router \\ nil) do
@@ -20,51 +21,58 @@ defmodule FastPostmanCollection.Collect do
     |> Enum.uniq()
     |> Enum.uniq_by(fn x -> x.plug end)
     |> Enum.map(&handle_generate_data(&1, router, list))
+    |> Enum.filter(&(not is_nil(&1)))
   end
 
   defp handle_generate_data(%{plug: plug}, router, all_routes) do
-    {_, _, _, _, doc, doc_params, functions} = Code.fetch_docs(plug)
-    functions_names = Enum.map(all_routes, & &1.plug_opts)
+    case Code.fetch_docs(plug) do
+      {_, _, _, _, doc, doc_params, functions} ->
+        functions_names = Enum.map(all_routes, & &1.plug_opts)
 
-    functions_list =
-      functions
-      |> Enum.filter(fn {{_, function_name, _}, _, _, _, _} ->
-        function_name in functions_names
-      end)
-      |> Enum.filter(fn {{_, _, _}, _, _, type, _} -> type != :hidden end)
-      |> Enum.filter(fn {{_, function_name, _}, _, _, _, _} ->
-        Enum.find(all_routes, fn x -> x.plug_opts == function_name and x.plug == plug end)
-        |> is_map()
-      end)
-      |> Enum.map(fn {{_, function_name, _}, _, _, doc, doc_params} ->
-        %{verb: verb, path: path} =
-          Enum.find(all_routes, fn x -> x.plug_opts == function_name and x.plug == plug end)
+        functions_list =
+          functions
+          |> Enum.filter(fn {{_, function_name, _}, _, _, _, _} ->
+            function_name in functions_names
+          end)
+          |> Enum.filter(fn {{_, _, _}, _, _, type, _} -> type != :hidden end)
+          |> Enum.filter(fn {{_, function_name, _}, _, _, _, _} ->
+            Enum.find(all_routes, fn x -> x.plug_opts == function_name and x.plug == plug end)
+            |> is_map()
+          end)
+          |> Enum.map(fn {{_, function_name, _}, _, _, doc, doc_params} ->
+            %{verb: verb, path: path} =
+              Enum.find(all_routes, fn x -> x.plug_opts == function_name and x.plug == plug end)
 
-        %CollectDataItem{
-          name: function_name,
-          method: verb,
-          route: path,
-          pipe_through:
-            FastPostmanCollection.Helpers.RouterInfo.route_info(
-              router,
-              Atom.to_string(verb)
-              |> String.upcase(),
-              path,
-              "h"
-            )[:pipe_through],
+            %CollectDataItem{
+              name: function_name,
+              method: verb,
+              route: path,
+              pipe_through:
+                FastPostmanCollection.Helpers.RouterInfo.route_info(
+                  router,
+                  Atom.to_string(verb)
+                  |> String.upcase(),
+                  path,
+                  "h"
+                )[:pipe_through],
+              title: documentation_handler(doc) |> title(),
+              documentation: documentation_handler(doc) |> documentation_body(),
+              doc_params: FastPostmanCollection.CollectDataItemParams.get_from_map(doc_params)
+            }
+          end)
+
+        %CollectDataModule{
+          module: plug,
+          doc_params: FastPostmanCollection.CollectDataModuleParams.get_from_map(doc_params),
           title: documentation_handler(doc) |> title(),
           documentation: documentation_handler(doc) |> documentation_body(),
-          doc_params: FastPostmanCollection.CollectDataItemParams.get_from_map(doc_params)
+          functions: functions_list
         }
-      end)
 
-    %CollectDataModule{
-      module: plug,
-      doc_params: FastPostmanCollection.CollectDataModuleParams.get_from_map(doc_params),
-      title: documentation_handler(doc) |> title(),
-      documentation: documentation_handler(doc) |> documentation_body(),
-      functions: functions_list
-    }
+      {:error, reason} ->
+        Logger.warning("this module have error - #{plug}, reason's error -> #{reason}")
+        nil
+    end
   end
 
   defp title(nil), do: nil
